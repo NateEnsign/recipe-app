494a6dc0b2f11fc1fef5d4aaac52c99edf3182fe
Nate Ensign <dixienate@gmail.com>
2024-12-19 07:49:12 -0700

    course goals basic format set up

---
+++ Desktop/funProjects/interview-mini-projects/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CourseGoals from './components/CourseGoals';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='main-header'>Course Goals</h1>
+      <CourseGoals />
+    </>
+  )
+}
+
+export default App
---
+++ Desktop/funProjects/interview-mini-projects/src/components/CourseGoals.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './CourseGoals.css';
+
+const CourseGoals = () => {
+  return (
+    <div>
+      <ul className="goal-list">
+        <li>Complete the course.</li>
+        <li>Help my friends out in the Q&A section</li>
+        <li>Go on to change the world</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CourseGoals;
